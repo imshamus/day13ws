9df56d0e66732ae893ef8765fe2b23f9bc1c99ed
imshamus <shamus-sjh@hotmail.com>
2024-11-28 01:01:44 +0800

    Added commands for task 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # day13ws
+mvn clean package
+java -jar target/day13ws-0.0.1-SNAPSHOT.jar --dataDir /Users/your-username/tmp/data
---
+++ src/main/java/sg/edu/nus/iss/day13ws/Day13wsApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day13ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day13wsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day13wsApplication.class, args);
+	}
+
+}
