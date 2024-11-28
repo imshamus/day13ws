6926c4694208e13619c3a11435a77180a2dcb819
imshamus <shamus-sjh@hotmail.com>
2024-11-29 00:19:25 +0800

    edit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # day13ws
+
+if no jar
+mvn spring-boot:run -Dspring-boot.run.arguments="--dataDir=/Users/your-username/tmp/data"
+
+for jar
 mvn clean package
 java -jar target/day13ws-0.0.1-SNAPSHOT.jar --dataDir /Users/your-username/tmp/data
+
--- src/main/java/sg/edu/nus/iss/day13ws/Day13wsApplication.java
+++ src/main/java/sg/edu/nus/iss/day13ws/Day13wsApplication.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss.day13ws;
 
+import java.nio.file.Paths;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Day13wsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Day13wsApplication.class, args);
+
+		// Parse arguments using DefaultApplicationArguments
+		ApplicationArguments appArgs = new DefaultApplicationArguments(args);
+
+		String dataDirPath = "";
+		String fileName = "";
+		// Check if dataDir is provided
+		if (appArgs.containsOption("dataDir"))
+		{
+			dataDirPath = 
+		}
+		
+
+		// Check that --dataDir argument is provided
+
+		// If dataDir not provided, stop the application with an error message
+
+		// Create the directory if it doesn't exist
+
 	}
 
 }
