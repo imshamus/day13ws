260543fceb6ef4483352f11f820f90e1500c288a
imshamus <shamus-sjh@hotmail.com>
2024-12-11 18:00:57 +0800

    completed task 1

--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 if no jar
 mvn spring-boot:run -Dspring-boot.run.arguments="--dataDir=/Users/your-username/tmp/data"
 
+
+
 for jar
 mvn clean package
 java -jar target/day13ws-0.0.1-SNAPSHOT.jar --dataDir /Users/your-username/tmp/data
 
+java -jar <name of jar file> --dataDir=“/users/sarahchan/Desktop/workshop 13” 
+quotes if got space in file names
+no quotes if no space
--- src/main/java/sg/edu/nus/iss/day13ws/Day13wsApplication.java
+++ src/main/java/sg/edu/nus/iss/day13ws/Day13wsApplication.java
@@ -1,6 +1,10 @@
 package sg.edu.nus.iss.day13ws;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.DefaultApplicationArguments;
@@ -12,17 +16,55 @@ public class Day13wsApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(Day13wsApplication.class, args);
+		SpringApplication app = new SpringApplication(Day13wsApplication.class);
+
+		// SpringApplication.run(Day13wsApplication.class, args);
 
 		// Parse arguments using DefaultApplicationArguments
 		ApplicationArguments appArgs = new DefaultApplicationArguments(args);
 
-		String dataDirPath = "";
-		String fileName = "";
+		String dataDir = ""; // initiliased the dir path variable
+		
 		// Check if dataDir is provided
 		if (appArgs.containsOption("dataDir"))
 		{
-			dataDirPath = 
+			// Checking if dataDir is provided from the command line
+			// getOptionValues("dataDir") returns a list of values associated with the --dataDir argument.
+			// java -jar myapp.jar --dataDir /path/to/dir1 --dataDir /path/to/dir2
+			// ["/path/to/dir1", "/path/to/dir2"]
+			// .get(0) retrieves first value of that list
+			dataDir = appArgs.getOptionValues("dataDir").get(0);
+			System.out.printf("Application will use data directory: %s\n", dataDir);
+
+			try 
+			{
+				// Creating the directory if it doesn't exist
+				Path dataDirPath = Paths.get(dataDir);
+				
+				if(Files.exists(dataDirPath)) // if file DNE
+				{
+					System.out.printf("Directory already exists at: %s \n", dataDirPath.toAbsolutePath().toString());
+				}
+				else
+				{
+					Files.createDirectories(dataDirPath); // Create parent directories if DNE
+					System.out.printf("Directory created at: %s \n", dataDirPath.toAbsolutePath().toString());
+				}
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+
+			// Start the Spring Boot application
+			app.run(args);
+		}
+
+		else
+
+		{
+			System.out.println("Can't start program, --dataDir argument is required.. ending..");
+			System.exit(1);
 		}
 		
 
